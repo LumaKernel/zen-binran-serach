b42b1cd6c0aed18b2078911070003fadfd5a42e2
Luma <world@luma.email>
2025-04-12 19:57:31 +0900

    typo fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://lumakernel.github.io/zen-binran-search/",
+  base: "https://lumakernel.github.io/zen-binran-serach/",
 });
